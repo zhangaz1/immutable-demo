b79716422ad5ab68179f6165e9681273d3dad8f5
zhangaz1 <zhangaz1@hotmail.com>
2018-09-20 15:35:24 +0800

    auto watch by node use npm start

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import * as Immutable from 'immutable';
 console.log('hello word!');
+
+var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+var map2 = map1.set('b', 2);
+console.assert(map1.equals(map2) === true);
+var map3 = map1.set('b', 50);
+console.assert(map1.equals(map3) === false);
